bf39ab18c3e097a49b5515505a4d71d9c88439e4
Giuditta <giudittamarino89@gmail.com>
2021-03-25 18:05:30 +0100

    esercizio terminato

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,41 +3,66 @@ var root = new Vue({
   data: {
     inputText: '',
     todos: [
-      {
-        title: 'Fare la spesa',
-        status: 'done'
-      },
-      {
-        title: 'Allenamento calcetto',
-        status: 'todo'
-      },
-      {
-        title: 'Leggere e-mail',
-        status: 'todo'
-      },
-      {
-        title: 'Telefonare medico',
-        status: 'todo'
-      },
-      {
-        title: 'Pagare bolletta',
-        status: 'todo'
-      }
+      // {
+      //   title: 'Fare la spesa',
+      //   status: 'done'
+      // },
+      // {
+      //   title: 'Allenamento calcetto',
+      //   status: 'todo'
+      // },
+      // {
+      //   title: 'Leggere e-mail',
+      //   status: 'todo'
+      // },
+      // {
+      //   title: 'Telefonare medico',
+      //   status: 'todo'
+      // },
+      // {
+      //   title: 'Pagare bolletta',
+      //   status: 'todo'
+      // }
     ]
   },
 
-  methods: {
-    // ALL'ENTER SULL'INPUT CREO UN OGGETTO CHE HA COME TITLE L'INPUTTEXT E LO PUSHO NELL'ARRAY DI TODOS
-    add: function () {
-      let obj = {
-        title: this.inputText,
-        status: 'todo'
-      }
-      this.todos.push(obj);
-      this.inputText = '';
+  computed: {
+    todosComputed: function() {
+      let todosTodo = this.todos.filter((todo) => todo.status == 'todo');
+      let todosDone = this.todos.filter((todo) => todo.status == 'done');
+
+      return [...todosTodo, ...todosDone];
     }
+  },
+
+  methods: {
+    // ALL'ENTER SULL'INPUT CREO UN OGGETTO CHE HA COME TITLE L'INPUTTEXT E LO PUSHO NELL'ARRAY DI TODOS, DOPO ASSEGNO STRINGA VUOTA AL VALUE INPUTTEXT
+    add: function () {
+      if (this.inputText != '') {
+        let obj = {
+          title: this.inputText,
+          status: 'todo'
+        }
+        this.todos.push(obj);
+        this.inputText = '';
+      }    
+    },
+
+    check: function(todo) {
+
+      let index = this.todos.indexOf(todo);
+      this.todos[index].status = 'done';
+    },
+
+    remove: function(todo) {
+
+      let index = this.todos.indexOf(todo);
+      console.log(index);
+      this.todos.splice(index, 1);
+    }
+
   }
 
 
 
-})
+});
